bad77fa76a523a39dca0cb24e0c52c1304f502f4
Billy <billybofh@gmail.com>
2019-11-07 18:29:03 +0000

    First stab at config file parsing

--- conmon.go
+++ conmon.go
@@ -3,20 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	"gopkg.in/yaml.v2"
 )
 
 type container struct {
 	image        string
 	lastDied     time.Time
+	lastAlerted  time.Time
 	restartCount int
 }
 
+type conf struct {
+	AlertDies      int `yaml:"alert_dies"`
+	AlertMinutes   int `yaml:"alert_minutes"`
+	AlertFrequency int `yaml:"alert_frequency"`
+}
+
 func main() {
+	configFilePath := os.Args[1]
+	c := conf{
+		AlertDies:      1,
+		AlertMinutes:   5,
+		AlertFrequency: 10,
+	}
+	if configFilePath != "" {
+		if _, err := os.Stat(configFilePath); err == nil {
+			yamlFile, err := ioutil.ReadFile(configFilePath)
+			if err != nil {
+				log.Printf("yamlFile.Get err   #%v ", err)
+			}
+			err = yaml.Unmarshal(yamlFile, &c)
+			if err != nil {
+				log.Fatalf("Unmarshal: %v", err)
+			}
+			fmt.Println(c)
+		}
+	}
 	containers := make(map[string]container)
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv)
@@ -45,8 +75,9 @@ func main() {
 			entry, cont := containers[msg.From]
 			if cont {
 				duration := time.Now().Sub(entry.lastDied).Minutes()
-				if duration < 5 && entry.restartCount > 1 {
+				if duration < 5 && entry.restartCount > 1 && time.Now().Sub(entry.lastAlerted).Minutes() > 10 {
 					fmt.Println("alert!")
+					entry.lastAlerted = time.Now()
 				}
 			}
 			if !cont {
